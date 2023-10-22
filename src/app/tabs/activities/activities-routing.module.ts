ed142e37db6a94183a8dbf7394e3fde71d5f7b62
Sebastián G <seba.gallardog@duocuc.cl>
2023-10-22 18:51:41 -0300

    qr page routing

--- src/app/tabs/activities/activities-routing.module.ts
+++ src/app/tabs/activities/activities-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ActivitiesPage } from './activities.page';
+import {authGuard} from "../../guards/auth.guard";
 
 const routes: Routes = [
   {
     path: '',
     component: ActivitiesPage,
+  },
+  {
+    path: 'qr',
+    loadChildren: () => import('./qr/qr.module').then( m => m.QrPageModule),
+    canActivate: [authGuard]
   }
 ];
 
