f67fc7b7fac7c92c5dd880289ceb88ec989fb4a3
Sebastián G <seba.gallardog@duocuc.cl>
2023-10-27 17:18:03 -0300

    crud

--- src/app/crud/create.service.ts
+++ src/app/crud/create.service.ts
@@ -6,23 +6,38 @@ import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class CrudService {
-  
+export class CreateService {
   private supabase: SupabaseClient;
 
-  constructor(
-  ) {
+  constructor() {
     this.supabase = createClient(
       environment.supabaseUrl,
       environment.supabaseKey
     );
-   }
+  }
 
-  async getHello() {
+  // Retorna true si el perfil del usuario esta completo
+  async isProfileCompleted(user_id: string):Promise<boolean> {
+    const { data, error } = await this.supabase
+      .from('user_profile')
+      .select('completed')
+      .eq('user_id', user_id);
+
+    if (error) {
+      console.error(error);
+      return false;
+    }
+    return data[0].completed;
+  }
+
+
+  async completeProfile() {
     const { data: todos, error } = await this.supabase.from('hello').select('*');
 
     console.log(todos);
     return todos;
   }
 
+
 }
+
