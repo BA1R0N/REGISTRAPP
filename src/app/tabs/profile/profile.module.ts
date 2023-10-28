297f425b6300d56b291a4c18b5f6e5ffd39210f9
Sebastián G <seba.gallardog@duocuc.cl>
2023-10-28 18:51:30 -0300

    Profile page

--- src/app/tabs/profile/profile.module.ts
+++ src/app/tabs/profile/profile.module.ts
@@ -1,24 +1,29 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProfilePage } from './profile.page';
 import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './profile-routing.module';
 import {ActionSheetComponent} from "../../components/action-sheet/action-sheet.component";
+import {Tab1PageModule} from "../home/home.module";
+import {CompleteProfileComponent} from "../../components/complete-profile/complete-profile.component";
+import {ProfileCardComponent} from "../../components/profile-card/profile-card.component";
 
 @NgModule({
-    imports: [
-        IonicModule,
-        CommonModule,
-        FormsModule,
-        ExploreContainerComponentModule,
-        Tab3PageRoutingModule
-    ],
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ExploreContainerComponentModule,
+    Tab3PageRoutingModule,
+    Tab1PageModule,
+    NgOptimizedImage
+  ],
     exports: [
         ActionSheetComponent
     ],
-    declarations: [ProfilePage, ActionSheetComponent]
+  declarations: [ProfilePage, ActionSheetComponent, CompleteProfileComponent, ProfileCardComponent]
 })
 export class Tab3PageModule {}
