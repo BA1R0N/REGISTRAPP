a9b2d1b7a22c9d6cb92d37d4816b0e643ccb8ee7
Sebastián G <seba.gallardog@duocuc.cl>
2023-10-28 20:32:21 -0300

    .

--- src/app/components/complete-profile/complete-profile.component.ts
+++ src/app/components/complete-profile/complete-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth/auth.service";
+import {AlertController, LoadingController} from "@ionic/angular";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-complete-profile',
@@ -7,8 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompleteProfileComponent  implements OnInit {
 
-  constructor() { }
+  credentials = this.fb.nonNullable.group({
+    first_name: ['', Validators.required],
+    last_name: ['', Validators.required],
+    role: ['', Validators.required],
+  })
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit() {}
+
+  async confirmCompletion() {
+
+  }
 
 }
