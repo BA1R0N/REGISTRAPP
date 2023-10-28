b9f453b958e03a3d77b8d817652e1af530d54fe0
Sebastián G <seba.gallardog@duocuc.cl>
2023-10-28 18:52:37 -0300

    crud

--- src/app/crud/create.service.ts
+++ src/app/crud/create.service.ts
@@ -16,26 +16,9 @@ export class CreateService {
     );
   }
 
-  // Retorna true si el perfil del usuario esta completo
-  async isProfileCompleted(user_id: string):Promise<boolean> {
-    const { data, error } = await this.supabase
-      .from('user_profile')
-      .select('completed')
-      .eq('user_id', user_id);
 
-    if (error) {
-      console.error(error);
-      return false;
-    }
-    return data[0].completed;
-  }
+  async completeProfile(first_name:string, last_name:string, user_type:string) {
 
-
-  async completeProfile() {
-    const { data: todos, error } = await this.supabase.from('hello').select('*');
-
-    console.log(todos);
-    return todos;
   }
 
 
--- src/app/crud/read.service.ts
+++ src/app/crud/read.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { createClient,SupabaseClient } from '@supabase/supabase-js'
+import { environment } from 'src/environments/environment';
+import {AuthService} from "../services/auth/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReadService {
+  private supabase: SupabaseClient;
+  user_id: string = this.authService.getCurrentUserId();
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+  ) {
+    this.supabase = createClient(
+        environment.supabaseUrl,
+        environment.supabaseKey
+    );
+  }
+
+
+  // Retorna true si el perfil del usuario esta completo
+  async isProfileCompleted():Promise<boolean> {
+    const { data, error } = await this.supabase
+        .from('user_profile')
+        .select('completed')
+        .eq('user_id', this.user_id);
+    if (error) {
+      console.error(error);
+      return false;
+    }
+    return data[0].completed;
+  }
+
+  // Retorna el nombre y apellido del usuario
+  async getNames() {
+    console.log('user_id: ', this.user_id)
+    const { data, error } = await this.supabase
+        .from('user_profile')
+        .select('first_name, last_name')
+        .eq('user_id', this.user_id);
+
+    if (error) {
+      console.error(error);
+      return false;
+    }
+
+    return data;
+  }
+
 }
