a432710e02c1ddaaaa1315cbab29738466a77673
Sebastián G <seba.gallardog@duocuc.cl>
2023-10-29 11:24:37 -0300

    Profile page

--- src/app/tabs/profile/profile.page.ts
+++ src/app/tabs/profile/profile.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
+import { ReadService } from "../../crud/read.service";
 
 @Component({
   selector: 'app-profile',
@@ -7,12 +8,12 @@ import { AuthService } from '../../services/auth/auth.service';
   styleUrls: ['profile.page.scss']
 })
 export class ProfilePage {
+  profileCompleted:boolean = false;
 
   constructor(
     private authService: AuthService,
-  ) {
-
-  }
+    private readService: ReadService,
+  ) { }
 
   // Returns true if the user id is not the default value
   isLoggedIn():boolean {
@@ -20,8 +21,23 @@ export class ProfilePage {
     return user_id !== '?';
   }
 
+  // Cierra la sesion del usuario
   async signOut() {
     await this.authService.signOut()
   }
+
+  ngOnInit() {
+    this.checkProfileCompleted().then(r =>
+      console.log('profileCompleted: ', this.profileCompleted)
+    );
+  }
+
+  async checkProfileCompleted() {
+    const user_id:string = this.authService.getCurrentUserId();
+    console.log('user_id: ', user_id)
+    this.profileCompleted = await this.readService.isProfileCompleted(user_id);
+    console.log('Completed?: ', this.profileCompleted)
+  }
+
 
 }
